af601e32abb8844484b68ded4624583d8a0e28d6
Dominik Mery <dominik@mery.online>
2023-02-09 12:48:40 +0100

    Add measure of update duration

--- src/Commands/UpdateDatabaseCommand.php
+++ src/Commands/UpdateDatabaseCommand.php
@@ -4,6 +4,9 @@ namespace Rockero\DatabaseUpdates\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Console\ConfirmableTrait;
+use Illuminate\Console\View\Components\Task;
+use Illuminate\Console\View\Components\Info;
+use Illuminate\Support\Benchmark;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use SplFileInfo;
@@ -12,58 +15,74 @@ class UpdateDatabaseCommand extends Command
 {
     use ConfirmableTrait;
 
+    protected $events;
+
     protected $signature = 'db:update {file?}
         {--force : Force the operation to run when in production}
         {--realpath : Indicate any provided migration file paths are pre-resolved absolute paths}';
 
     protected $description = 'Run database updates';
 
-    public function handle()
+    public function handle(): int
     {
         if (! $this->confirmToProceed()) {
             return Command::FAILURE;
         }
 
-        if ($this->argument('file')) {
-            $this->comment('Updating: '.$this->argument('file'));
-
-            $path = sprintf('%s/%s.php', $this->getPath(), $this->argument('file'));
-
-            File::getRequire($path)->__invoke($this->output);
-
-            $this->info('Done');
-
-            return Command::SUCCESS;
-        }
-
-        $updates = collect(File::files($this->getPath()));
+        $updates = collect($this->getUpdateFiles());
 
         $executedUpdates = DB::table('database_updates')->pluck('file')->all();
 
         $pendingUpdates = $updates->reject(fn (SplFileInfo $file) => in_array($file->getFilename(), $executedUpdates));
 
+        if ($pendingUpdates->count() == 0) {
+            $this->write(Info::class, 'Nothing to update.');
+            return Command::SUCCESS;
+        }
+
+        $this->write(Info::class, 'Running updates.');
+
         $pendingUpdates->each(function (SplFileInfo $file) {
-            $filename = $file->getFilename();
-
-            $this->comment("Updating: {$filename}");
-
-            File::getRequire($file)->__invoke($this->output);
-
-            DB::table('database_updates')->insert([
-                'file' => $file->getFilename(),
-                'executed_at' => now()->toDateTimeString(),
-            ]);
-
-            $this->info("Updated: {$filename}");
+            $this->write(Task::class, $file->getFilename(), fn () => $this->runUpdate($file));
         });
+
+        $this->output->writeln('');
 
         return Command::SUCCESS;
     }
 
-    public function getPath()
+    public function getPath(): string
     {
         return $this->input->hasOption('realpath') && $this->option('realpath')
             ? $this->option('realpath')
             : database_path('updates');
+    }
+
+    public function getUpdateFiles(): array
+    {
+        if ($this->argument('file')) {
+            $path = sprintf('%s/%s.php', $this->getPath(), $this->argument('file'));
+            $file = new SplFileInfo($path);
+            return [$file];
+        } else {
+            return File::files($this->getPath());
+        }
+    }
+
+    protected function runUpdate(SplFileInfo $update): void
+    {
+        Benchmark::measure(function () use ($update) {
+            File::getRequire($update)->__invoke();
+
+            DB::table('database_updates')->insert([
+                'file' => $update->getFilename(),
+                'executed_at' => now()->toDateTimeString(),
+            ]);
+        });
+    }
+
+    protected function write(string $component, ...$arguments): void
+    {
+        (new $component($this->output))->render(...$arguments);
     }
 }
